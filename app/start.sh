8c39c388d51f989d1a8bf4e6b36f14a6dd7abbca
Ryan Seo <hseo22@gmail.com>
2017-09-15 21:13:50 -0700

    run typed-for-css-modules with watch option, when running make run

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "************ Generate CSS typings file ************"
+node node_modules/typed-css-modules/lib/cli ClientApp -w &
+
 echo "************ Building Webpack Bundled Scripts ************"
 node node_modules/webpack/bin/webpack.js --config webpack.config.vendor.js
 node node_modules/webpack/bin/webpack.js
