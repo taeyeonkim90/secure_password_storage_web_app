95b138bbae484c929c75d05bd2f14554cb36d034
Andrew Kim <taeyeonkim90@gmail.com>
2017-10-13 15:57:12 -0700

    added placeholder attribute in the input box area for new Cards, and updating Cards

--- app/ClientApp/components/FieldInput/FieldInput.tsx
+++ app/ClientApp/components/FieldInput/FieldInput.tsx
@@ -3,6 +3,22 @@ import { RouteComponentProps } from 'react-router-dom';
 import * as css from './FieldInput.css';
 
 const FieldInput = (props) => {
+    var placeholder = ""
+    switch (props.name) {
+        case ("accountName"):
+            placeholder = "Facebook, Gmail, etc."
+            break;
+        case ("userName"):
+            placeholder = "example@example.com"
+            break;
+        case ("pw"):
+            placeholder = "your password"
+            break;
+        case ("description"):
+            placeholder = "descriptions for your information"
+            break;
+    }
+
     return (
         <div>
             <p> {props.displayName}: </p>
@@ -10,6 +26,7 @@ const FieldInput = (props) => {
                 type={props.type}
                 name={props.name}
                 value={props.data} 
+                placeholder={placeholder}
                 onChange={props.updateState}/>
         </div>
         )
