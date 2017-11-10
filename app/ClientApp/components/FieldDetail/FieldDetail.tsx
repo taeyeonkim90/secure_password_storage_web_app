9e0167184c3d8dd5180760f080a84ea44a8deb18
Andrew Kim <taeyeonkim90@gmail.com>
2017-11-09 17:44:27 -0800

    put some margin between top bar, horizontal logo, and the search bar
    modified FieldDetail component to hide password information when folded card is clicked

--- app/ClientApp/components/FieldDetail/FieldDetail.tsx
+++ app/ClientApp/components/FieldDetail/FieldDetail.tsx
@@ -5,13 +5,18 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import { toast } from 'react-toastify';
 
 const FieldDetail = (props) => {
+    var message = `Copied "${props.data}" to the clipboard`
+    if (props.hidePassword){
+        message = `Copied your password for "${props.display}" to the clipboard`
+    }
+
     return (
         <div>
-            <CopyToClipboard text={props.data} onCopy={()=>toast(`Copied ${props.data} to the clipboard`)}>
+            <CopyToClipboard text={props.data} onCopy={()=>toast(message)}>
                 <p className={css.fieldDetail} > {props.display} </p>
             </CopyToClipboard>
         </div>
-        )
+    )
 }
 
 export default FieldDetail;
